a6ad9645407e6097cb47ae39366e26ac44f6f12d
Matko <matkobusljeta@gmail.com>
2021-10-31 19:05:40 +0100

    Added some settings to init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,8 @@
 require('plugins')
 require('treesitter-config')
-vim.wo.number = true
-vim.cmd('colorscheme nord')
+
+vim.wo.number = true   		--display line numbers
+vim.o.mouse = "a" 		--enable mouse in all modes
+vim.wo.cursorline = true	--highlight cursor line
+
+vim.cmd('colorscheme nord')	--set colorscheme to nord
