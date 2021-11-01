74e3f39898a4c32280232f025958ca83206a07f7
Matko <matkobusljeta@gmail.com>
2021-11-01 19:15:54 +0100

    Added barbar tabline plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,3 +9,6 @@ vim.o.mouse = "a" 		--enable mouse in all modes
 vim.wo.cursorline = true	--highlight cursor line
 
 vim.cmd('colorscheme gruvbox')	--set colorscheme to gruvbox
+
+--vim.cmd('NvimTreeOpen')		--auto open filetree
+--vim.cmd('vsplit')		--auto split window
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -22,10 +22,14 @@ return require('packer').startup(function()
 
     --- Nvim tree file explorer
     use {
-    'kyazdani42/nvim-tree.lua',
-    requires = 'kyazdani42/nvim-web-devicons',
-    config = function() require'nvim-tree'.setup {} end
-}
+    	'kyazdani42/nvim-tree.lua',
+    	requires = 'kyazdani42/nvim-web-devicons',
+    	config = function() require'nvim-tree'.setup {} end
+    }
 
-
+    --- Barbar tabline
+    use {
+  	'romgrk/barbar.nvim',
+  	requires = {'kyazdani42/nvim-web-devicons'}
+    }
 end)
