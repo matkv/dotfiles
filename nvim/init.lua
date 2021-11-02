31f377888aaf2fde8d096e6ca62a7e43f4065079
Matko <matkobusljeta@gmail.com>
2021-11-02 21:25:48 +0100

    Started setting up auto layout on startup

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,8 +7,14 @@ require('nvim-web-devicons-config')
 vim.wo.number = true   		--display line numbers
 vim.o.mouse = "a" 		--enable mouse in all modes
 vim.wo.cursorline = true	--highlight cursor line
+vim.o.splitbelow = false	--disable auto splitting below
+vim.o.splitright = true 	--split to the right when using :split command
+vim.o.wrap = false
 
 vim.cmd('colorscheme gruvbox')	--set colorscheme to gruvbox
 
---vim.cmd('NvimTreeOpen')		--auto open filetree
---vim.cmd('vsplit')		--auto split window
+vim.cmd[[au VimEnter * NvimTreeOpen]]
+vim.cmd[[au VimEnter * wincmd p]]
+vim.cmd[[au VimEnter * terminal]]
+vim.cmd[[au VimEnter * wincmd k]]
+vim.cmd[[au VimEnter * vsplit new]]
