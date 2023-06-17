df890bd2c9fbba9372f435c81a2f8ae0d1ddc8ab
matkv <mail@matkv.dev>
2023-06-17 23:56:21 +0200

    removed a theme I wasnt using

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   { "rcarriga/nvim-notify", enabled = false },
   { "echasnovski/mini.indentscope", enabled = false },
   { "folke/noice.nvim", enabled = false },
+  { "folke/tokyonight.nvim", enabled = false },
 }
