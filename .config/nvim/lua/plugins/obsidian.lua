860486aea872c7f88494e7cc75b8f748665d3c5c
matkv <mail@matkv.dev>
2023-07-10 20:26:34 +0200

    initial config of obsidian plugin for neovim

---
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,47 @@
+return {
+  "epwalsh/obsidian.nvim",
+  lazy = true,
+  event = { "BufReadPre " .. vim.fn.expand("~") .. "/documents/obsidian/**.md" },
+  dependencies = {
+    -- Required.
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    dir = "~/documents/obsidian/", -- no need to call 'vim.fn.expand' here
+
+    -- Optional, completion.
+    completion = {
+      -- If using nvim-cmp, otherwise set to false
+      nvim_cmp = true,
+      -- Trigger completion at 2 chars
+      min_chars = 1,
+      -- Where to put new notes created from completion. Valid options are
+      --  * "current_dir" - put new notes in same directory as the current buffer.
+      --  * "notes_subdir" - put new notes in the default notes subdirectory.
+      new_notes_location = "current_dir",
+    },
+
+    -- Optional, set to true if you don't want Obsidian to manage frontmatter.
+    disable_frontmatter = true,
+
+    -- Optional, for templates (see below).
+    templates = {
+      subdir = "templates",
+      date_format = "%Y-%m-%d-%a",
+      time_format = "%H:%M",
+    },
+  },
+  config = function(_, opts)
+    require("obsidian").setup(opts)
+
+    -- Optional, override the 'gf' keymap to utilize Obsidian's search functionality.
+    -- see also: 'follow_url_func' config option above.
+    vim.keymap.set("n", "gf", function()
+      if require("obsidian").util.cursor_on_markdown_link() then
+        return "<cmd>ObsidianFollowLink<CR>"
+      else
+        return "gf"
+      end
+    end, { noremap = false, expr = true })
+  end,
+}
