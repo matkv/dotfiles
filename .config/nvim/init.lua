d3971fa7dc79a0f13a3b113a7a008436c09db129
matkv <40850821+matkv@users.noreply.github.com>
2023-07-17 09:17:03 +0200

    set custom config for Neovide

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- custom config when running on Windows (Neovide)
+require("config.neovide")
---
+++ .config/nvim/lua/config/neovide.lua
@@ -0,0 +1,3 @@
+if vim.g.neovide then
+	vim.o.guifont = "FiraCode Nerd Font:h10" -- text below applies for VimScript
+end
