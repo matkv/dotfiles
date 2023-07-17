f06fc6c5fce2e82f3e545271ac00717536ca946f
matkv <40850821+matkv@users.noreply.github.com>
2023-07-17 10:02:55 +0200

    disable cursor animations for Neovide

--- .config/nvim/lua/config/neovide.lua
+++ .config/nvim/lua/config/neovide.lua
@@ -1,3 +1,8 @@
 if vim.g.neovide then
-	vim.o.guifont = "FiraCode Nerd Font:h10" -- text below applies for VimScript
+  -- set font
+  vim.o.guifont = "FiraCode Nerd Font:h10" -- text below applies for VimScript
+
+  -- disable cursor animation
+  vim.g.neovide_cursor_animation_length = 0
+  vim.g.neovide_cursor_trail_size = 0
 end
