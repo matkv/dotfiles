774b672f93c77c0fd507ac6b7c0d883145338a31
matkv <mail@matkv.dev>
2024-01-05 00:45:04 +0100

    switched wezterm font to FiraCode and use Gruvbox theme

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
@@ -7,13 +7,14 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'AdventureTime'
+config.color_scheme = "Gruvbox dark, medium (base16)"
+config.font = wezterm.font("Fira Code")
 
 -- and finally, return the configuration to wezterm
 return config
