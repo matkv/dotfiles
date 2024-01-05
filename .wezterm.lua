907a1c8d08b8433d71da30a700a15869c0a7c375
matkv <mail@matkv.dev>
2024-01-05 22:43:08 +0100

    set wezterm tab bar and window decoration

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,21 +1,16 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
-
--- This table will hold the configuration.
 local config = {}
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
 config.color_scheme = "Gruvbox dark, medium (base16)"
 config.font = wezterm.font("Fira Code")
 config.font_size = 11.0
 
--- and finally, return the configuration to wezterm
+config.tab_bar_at_bottom = true
+config.window_decorations = "RESIZE"
+
 return config
+
