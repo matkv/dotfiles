63e8bc8de5cd25c157c935c83b248ff271086196
matkv <mail@matkv.dev>
2024-04-10 21:37:44 +0200

    tried and failed to setup nvim roslyn

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+-- config location on windows -> C:\Users\matko\AppData\Local\nvim
 -- custom config when running on Windows (Neovide)
 require("config.neovide")
+
