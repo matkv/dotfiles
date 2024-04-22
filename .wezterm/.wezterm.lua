fd0d3a78b6ebf7430a926ed6e476948eca1211f6
matkv <mail@matkv.dev>
2024-04-22 22:48:32 +0200

    made wezterm more gruvboxy

--- .wezterm/.wezterm.lua
+++ .wezterm/.wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local config = {}
 
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 config.color_scheme = "Gruvbox dark, medium (base16)"
@@ -12,5 +12,29 @@ config.font_size = 11.0
 config.tab_bar_at_bottom = true
 config.window_decorations = "RESIZE"
 
-return config
+config.window_frame = {
+	active_titlebar_bg = "#363636",
+}
 
+config.colors = {
+	tab_bar = {
+		active_tab = {
+			fg_color = "#A5A41E",
+			bg_color = "#464240",
+		},
+		inactive_tab = {
+			fg_color = "#A5A41E",
+			bg_color = "#363636",
+		},
+		inactive_tab_hover = {
+			fg_color = "#E86612",
+			bg_color = "#363636",
+		},
+		new_tab_hover = {
+			fg_color = "#E86612",
+			bg_color = "#363636",
+		},
+	},
+}
+
+return config
