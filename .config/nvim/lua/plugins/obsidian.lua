d82c56a49273c4710d56bc5254ce20d7f58b4f70
matkv <mail@matkv.dev>
2024-04-26 20:54:05 +0200

    added which key options for obsidian plugin

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -6,9 +6,88 @@ return {
   dependencies = {
     -- Required.
     "nvim-lua/plenary.nvim",
-
-    -- see below for full list of optional dependencies 👇
   },
+  config = function(_, opts)
+    -- Setup obsidian.nvim
+    require("obsidian").setup(opts)
+
+    -- Create which-key mappings for common commands.
+    local wk = require "which-key"
+
+    wk.register {
+      ["<leader>o"] = {
+        name = "Obsidian",
+        o = { "<cmd>ObsidianOpen<cr>", "Open note" },
+        d = { "<cmd>ObsidianDailies -10 0<cr>", "Daily notes" },
+        p = { "<cmd>ObsidianPasteImg<cr>", "Paste image" },
+        q = { "<cmd>ObsidianQuickSwitch<cr>", "Quick switch" },
+        s = { "<cmd>ObsidianSearch<cr>", "Search" },
+        t = { "<cmd>ObsidianTags<cr>", "Tags" },
+        l = { "<cmd>ObsidianLinks<cr>", "Links" },
+        b = { "<cmd>ObsidianBacklinks<cr>", "Backlinks" },
+        -- b = { "<cmd>luafile lua/backlinks.lua<cr>", "Backlinks" },
+        m = { "<cmd>ObsidianTemplate<cr>", "Template" },
+        n = { "<cmd>ObsidianQuickSwitch nav<cr>", "Nav" },
+        r = { "<cmd>ObsidianRename<cr>", "Rename" },
+        w = { "<cmd>ObsidianWorkspace<cr>", "Workspace" },
+        c = {
+          function()
+            local day_of_week = os.date "%A"
+            assert(type(day_of_week) == "string")
+
+            ---@type integer
+            local offset_start
+            if day_of_week == "Sunday" then
+              offset_start = 1
+            elseif day_of_week == "Monday" then
+              offset_start = 0
+            elseif day_of_week == "Tuesday" then
+              offset_start = -1
+            elseif day_of_week == "Wednesday" then
+              offset_start = -2
+            elseif day_of_week == "Thursday" then
+              offset_start = -3
+            elseif day_of_week == "Friday" then
+              offset_start = -4
+            elseif day_of_week == "Saturday" then
+              offset_start = 2
+            end
+            assert(offset_start)
+
+            vim.cmd(string.format("ObsidianDailies %d %d", offset_start, offset_start + 4))
+          end,
+          "Current week",
+        },
+      },
+    }
+
+    wk.register({
+      ["<leader>o"] = {
+        name = "Obsidian",
+        e = {
+          function()
+            local title = vim.fn.input { prompt = "Enter title (optional): " }
+            vim.cmd("ObsidianExtractNote " .. title)
+          end,
+          "Extract text into new note",
+        },
+        l = {
+          function()
+            vim.cmd "ObsidianLink"
+          end,
+          "Link text to an existing note",
+        },
+        n = {
+          function()
+            vim.cmd "ObsidianLinkNew"
+          end,
+          "Link text to a new note",
+        },
+      },
+    }, {
+      mode = "v",
+    })
+  end,
   opts = {
     workspaces = {
       {
