449bfe6513460baf680f9b03c0fcb75a4210c64e
matkv <mail@matkv.dev>
2024-06-19 17:32:24 +0200

    adapt font when using neovide

--- .config/nvim/lua/config/neovide.lua
+++ .config/nvim/lua/config/neovide.lua
@@ -1,6 +1,6 @@
 if vim.g.neovide then
   -- set font
-  vim.o.guifont = "FiraCode Nerd Font:h10"
+  vim.o.guifont = "Fira Code:h11"
 
   -- disable cursor animation
   vim.g.neovide_cursor_animation_length = 0
