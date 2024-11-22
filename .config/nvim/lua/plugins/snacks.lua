748e9f46656e4c669b3d6790086c04d3e9f62b22
matkv <mail@matkv.dev>
2024-11-22 19:56:43 +0100

    initial setup of snacks plugin + removed obsolete plugins

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,19 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    bigfile = { enabled = true },
+    bufdelete = { enabled = true },
+    dashboard = { enabled = true },
+    git = { enabled = true },
+    lazygit = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    rename = { enabled = true },
+    statuscolumn = { enabled = true },
+    terminal = { enabled = true },
+    toggle = { enabled = true },
+    words = { enabled = true },
+  },
+}
