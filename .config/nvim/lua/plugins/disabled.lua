02f7739211a6184e85bb888aaeb2ae5f0c338e2b
matkv <mail@matkv.dev>
2025-09-22 20:59:09 +0200

    clean up neovim config after update to lazyvim 15

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,8 +1,8 @@
 return {
-  { "rcarriga/nvim-notify", enabled = false },
-  { "echasnovski/mini.indentscope", enabled = false },
-  { "folke/noice.nvim", enabled = false },
-  { "folke/tokyonight.nvim", enabled = false },
-  { "ggandor/leap.nvim", enabled = false },
-  { "catppuccin/nvim", enabled = false },
+  { "rcarriga/nvim-notify",       enabled = false },
+  { "nvim-mini/mini.indentscope", enabled = false },
+  { "folke/noice.nvim",           enabled = false },
+  { "folke/tokyonight.nvim",      enabled = false },
+  { "ggandor/leap.nvim",          enabled = false },
+  { "catppuccin/nvim",            enabled = false },
 }
