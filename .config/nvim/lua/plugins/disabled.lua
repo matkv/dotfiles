cd7c7a4eaf881eaa64111e4fb2cfe0adc5db8316
matkv <mail@matkv.dev>
2026-03-07 22:32:34 +0100

    remove leap from disabled nvim plugins list

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -3,6 +3,5 @@ return {
   { "nvim-mini/mini.indentscope", enabled = false },
   { "folke/noice.nvim",           enabled = false },
   { "folke/tokyonight.nvim",      enabled = false },
-  { "ggandor/leap.nvim",          enabled = false },
   { "catppuccin/nvim",            enabled = false },
 }
