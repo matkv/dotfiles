e3304f315376854d256caa276ca28c0331c0cbf9
matkv <mail@matkv.dev>
2026-04-02 23:24:40 +0200

    add nvim snacks picker and snacks explorer

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -257,26 +257,32 @@ end
 -- the rest of the lines are just quality-of-life/appearance plugins and
 -- can be removed.
 
--- INFO: fuzzy finder
+-- INFO: snacks.nvim - picker (fuzzy finder) + explorer (file tree)
 vim.pack.add({
-  "https://github.com/nvim-lua/plenary.nvim",        -- library dependency
-  "https://github.com/nvim-tree/nvim-web-devicons",  -- icons (nerd font)
-  "https://github.com/nvim-telescope/telescope.nvim" -- the fuzzy finder
+  "https://github.com/nvim-tree/nvim-web-devicons", -- icons (nerd font)
+  "https://github.com/folke/snacks.nvim",
 }, { confirm = false })
 
-require("telescope").setup({})
+require("snacks").setup({
+  picker = { enabled = true },
+  explorer = { enabled = true },
+})
 
-local pickers = require("telescope.builtin")
+local pick = require("snacks").picker
 
-vim.keymap.set("n", "<leader>sp", pickers.builtin, { desc = "[S]earch Builtin [P]ickers", })
-vim.keymap.set("n", "<leader>sb", pickers.buffers, { desc = "[S]earch [B]uffers", })
-vim.keymap.set("n", "<leader>sf", pickers.find_files, { desc = "[S]earch [F]iles", })
-vim.keymap.set("n", "<leader>sw", pickers.grep_string, { desc = "[S]earch Current [W]ord", })
-vim.keymap.set("n", "<leader>sg", pickers.live_grep, { desc = "[S]earch by [G]rep", })
-vim.keymap.set("n", "<leader>sr", pickers.resume, { desc = "[S]earch [R]esume", })
+vim.keymap.set("n", "<leader>sp", function() pick.pickers() end,   { desc = "[S]earch Builtin [P]ickers" })
+vim.keymap.set("n", "<leader>sb", function() pick.buffers() end,   { desc = "[S]earch [B]uffers" })
+vim.keymap.set("n", "<leader>sf", function() pick.files() end,     { desc = "[S]earch [F]iles" })
+vim.keymap.set("n", "<leader>sw", function() pick.grep_word() end, { desc = "[S]earch Current [W]ord" })
+vim.keymap.set("n", "<leader>sg", function() pick.grep() end,      { desc = "[S]earch by [G]rep" })
+vim.keymap.set("n", "<leader>sr", function() pick.resume() end,    { desc = "[S]earch [R]esume" })
+vim.keymap.set("n", "<leader>sh", function() pick.help() end,      { desc = "[S]earch [H]elp" })
+vim.keymap.set("n", "<leader>sm", function() pick.man() end,       { desc = "[S]earch [M]anuals" })
 
-vim.keymap.set("n", "<leader>sh", pickers.help_tags, { desc = "[S]earch [H]elp", })
-vim.keymap.set("n", "<leader>sm", pickers.man_pages, { desc = "[S]earch [M]anuals", })
+vim.keymap.set("n", "<leader>e", function() require("snacks").explorer() end,
+  { desc = "Toggle [E]xplorer" })
+vim.keymap.set("n", "<leader>E", function() require("snacks").explorer({ reveal = true }) end,
+  { desc = "Reveal file in [E]xplorer" })
 
 -- INFO: better statusline
 vim.pack.add({ "https://github.com/nvim-lualine/lualine.nvim" }, { confirm = false })
@@ -285,6 +291,9 @@ require("lualine").setup({
   options = {
     section_separators = { left = "", right = "", },
     component_separators = { left = "", right = "", },
+    disabled_filetypes = {
+      statusline = { "snacks_picker_list", "snacks_picker_input", "snacks_layout_box" },
+    },
   },
 })
 
@@ -319,7 +328,7 @@ require("bufferline").setup({
     show_close_icon = false,
     separator_style = "thin",
     offsets = {
-      { filetype = "NvimTree", text = "Files", highlight = "Directory", text_align = "left" },
+      { filetype = "snacks_layout_box", text = "Files", highlight = "Directory", text_align = "left" },
     },
   },
 })
