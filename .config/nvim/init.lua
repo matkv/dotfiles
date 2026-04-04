dca8974d3407563d36bf9f58884ba1987badc20b
matkv <mail@matkv.dev>
2026-04-04 23:19:41 +0200

    add nvim flash plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -221,4 +221,24 @@ vim.pack.add({
 require("nvim-autopairs").setup()
 require("todo-comments").setup()
 
+vim.pack.add({ "https://github.com/folke/flash.nvim" }, { confirm = false })
+
+require("flash").setup({})
+
+vim.keymap.set({ "n", "x", "o" }, "s", function()
+	require("flash").jump()
+end, { desc = "Flash" })
+vim.keymap.set({ "n", "x", "o" }, "S", function()
+	require("flash").treesitter()
+end, { desc = "Flash Treesitter" })
+vim.keymap.set("o", "r", function()
+	require("flash").remote()
+end, { desc = "Remote Flash" })
+vim.keymap.set({ "o", "x" }, "R", function()
+	require("flash").treesitter_search()
+end, { desc = "Treesitter Search" })
+vim.keymap.set("c", "<C-s>", function()
+	require("flash").toggle()
+end, { desc = "Toggle Flash Search" })
+
 -- TODO maybe add trouble.nvim
